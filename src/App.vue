a6fb78f46e099e86f9238428432b791e352e5fd2
CarloColletti~ <carlocolletti5@gmail.com>
2023-02-27 17:24:46 +0100

    prime regole

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+//import
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Main from './components/Main.vue';
+
+//export
+export default {
+  components: {
+    Header,
+    Footer,
+    Main
+  }
+
+}
 </script>
 
-<template></template>
+<template>
+  <!-- stampo su html -->
+    <Header />
+    <Main />
+    <Footer />
+</template>
 
 <style>
-* {
+/* regole base  */
+*{
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 
-img {
+img{
   max-width: 100%;
 }
 
 ul {
   list-style-type: none;
 
-  li {
+  li{
     margin: 0 0.5rem;
 
-    a {
+    a{
       color: inherit;
       text-decoration: none;
     }
   }
 }
+
 </style>
