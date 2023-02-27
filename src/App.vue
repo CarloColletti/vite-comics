23377de8e4903b0e54d93c1272773000dd8ad118
CarloColletti~ <carlocolletti5@gmail.com>
2023-02-27 17:06:39 +0100

    add image

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,27 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template></template>
 
-<style></style>
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+img {
+  max-width: 100%;
+}
+
+ul {
+  list-style-type: none;
+
+  li {
+    margin: 0 0.5rem;
+
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+  }
+}
+</style>
